18ca386e967bdccb058cfef1b99efe8b96fa60cc
rolandfenyes <ftendalan@gmail.com>
2021-02-19 14:59:31 +0100

    it works

--- src/app/services/product-service.service.ts
+++ src/app/services/product-service.service.ts
@@ -9,12 +9,12 @@ export class ProductServiceService {
 
   productUrl: string;
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
-    this.productUrl = '/api/playgrounds';
+  constructor(private http: HttpClient, private baseUrl: string) {
+    this.productUrl = '/api/playground';
   }
 
   async getProducts(): Promise<ProductDTO[]> {
-    return this.http.get<ProductDTO[]>(this.baseUrl + this.productUrl + '/getAll').toPromise();
+    return this.http.get<ProductDTO[]>( 'http://192.168.0.110:8080' + this.productUrl + '/getAll').toPromise();
   }
 
 }
